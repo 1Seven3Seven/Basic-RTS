198ac8a729008eddb7fdf5fd62bb72337bbf5b69
Peter Burgess <pburgess2003@gmail.com>
2023-01-20 19:55:20 +1030

    Work on iPad. Better spelling More notes

--- Environment.py
+++ Environment.py
@@ -1,10 +1,20 @@
 """
 The environment is grid based.
 
-Environment objects
-    Two resources will spawn:
+Environment objects:
+    Terrain:
+        Hills
+        Mountains
+        Rivers
+
+    The resources that will spawn:
         Trees
         Stone Despots
+        
+    Player bases (unchangeable):
+        Player 1 base
+        Player 2 base
+        ...
 
     The environment can be modified by placing structures:
         Wood walls
@@ -14,22 +24,42 @@ Environment objects
         Stone spawner
 
 
-Environment weights based on objects:
-    Clear node:         1
+Node weights based on objects:
+    Train:
+        Clear node:             1
+        
+        Hill node:              6
+        Mountain node:          20
+        River node:             10
 
-    Tree node:          3
-    Stone node:         3
-
-    Wood wall node:     7
-    Stone wall node:    12
-
-    Wood spawner node:  1
-    Stone spawner node: 1
+    Structures:
+        Tree node:              2
+        Stone node:             2
+        
+        Player x base node:     1
+    
+        Wood wall node:         8
+        Stone wall node:        12
+    
+        Wood spawner node:      1
+        Stone spawner node:     1
+        
+Stone deposits are much more likely on mountains and hills.
+Trees and stone deposits are impossible on river.
+    
+The player bases are a 2x2 object, each one can spawn one worker.
 
 When a node is changed the weights of all connections are changed.
 The weight for a connection between two nodes is the largest weight of the two according to the above info.
+    By that I mean the candidate weights are the sum of the terrain and structure weight.
 
 When entities move from node to node their speed depends on the weight between those nodes.
+
+Around a Player base, there should be a minimum number of trees and stone deposits.
+There will be a minimum range for stone deposits.
+e.g.
+    A minimum of 100 trees will spawn in a 30 node radius.
+    A minimum of 3 stone deposits will spawn between 20 and 45 nodes away.
 """
 
 from enum import Enum, auto
@@ -37,15 +67,38 @@ from enum import Enum, auto
 from AStar import Node, NodeGenerator
 
 
-class GridSquareTypes(Enum):
+class GridSquareTerrain(Enum):
     """
-The state that each grid square can be.
+The terrain that each grid square can have.
+    """
+    
+    CLEAR = auto()
+    
+    HILL = auto()
+    MOUNTAIN = auto()
+    RIVER = auto()
+    
+
+TerrainToWeight = {
+    GridSquareTerrain.CLEAR: 1,
+    
+    GridSquareTerrain.HILL: 6,
+    GridSquareTerrain.MOUNTAIN: 20,
+    GridSquareTerrain.RIVER: 10
+}
+
+
+class GridSquareStructures(Enum):
+    """
+The structures that each grid can have.
     """
 
-    CLEAR = auto()
+    NONE = auto()
 
     TREE = auto()
     STONE = auto()
+    
+    PLAYER_BASE = auto()
 
     WOOD_WALL = auto()
     STONE_WALL = auto()
@@ -54,17 +107,19 @@ The state that each grid square can be.
     STONE_SPAWNER = auto()
 
 
-TypeToWeight = {
-    GridSquareTypes.CLEAR: 1,
+StructureToWeight = {
+    GridSquareStructures.NONE: 1,
 
-    GridSquareTypes.TREE: 3,
-    GridSquareTypes.STONE: 3,
+    GridSquareStructures.TREE: 3,
+    GridSquareStructures.STONE: 3,
+    
+    GridSquareStructures.PLAYER_BASE: 1,
 
-    GridSquareTypes.WOOD_WALL: 7,
-    GridSquareTypes.STONE_WALL: 12,
+    GridSquareStructures.WOOD_WALL: 7,
+    GridSquareStructures.STONE_WALL: 12,
 
-    GridSquareTypes.WOOD_SPAWNER: 1,
-    GridSquareTypes.STONE_SPAWNER: 1
+    GridSquareStructures.WOOD_SPAWNER: 1,
+    GridSquareStructures.STONE_SPAWNER: 1
 }
 
 
@@ -75,14 +130,17 @@ A node that contains information about the state of the grid square.
 
     def __init__(self, x_position: int, y_position: int):
         super().__init__(x_position, y_position)
+        
+        # The terrain of the grid square
+        self.terrain: GridSquareTerrain = GridSquareTerrain.CLEAR
 
-        # The current type of the grid square
-        self.type: GridSquareTypes = GridSquareTypes.CLEAR
+        # The current structure of the grid square
+        self.structure: GridSquareStructures = GridSquareStructures.NONE
 
 
 class Environment:
-    def __init__(self, x_width: int, y_width: int):
-        self.grid = NodeGenerator.Grid(x_width, y_width, node_class=GridSquare)
+    def __init__(self, width: int, height: int):
+        self.grid: NodeGenerator.Grid = NodeGenerator.Grid(width, height, node_class=GridSquare)
 
     def __getitem__(self, coords: tuple[int, int]) -> GridSquare:
         """
@@ -104,3 +162,4 @@ Returns the GridSquare at the corresponding coordinates.
         return self.grid.y_size
 
     # endregion - Properties
+
