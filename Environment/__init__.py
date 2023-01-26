3dce79097d79cdfe32c4b0e30807d57bfe20c441
Peter Burgess <pburgess2003@gmail.com>
2023-01-26 21:09:43 +1030

    Update to Environment/__init__.py.

--- Environment/__init__.py
+++ Environment/__init__.py
@@ -1,8 +1,15 @@
 # Must be first
 from .GridSquare import GridSquare
 
+# Must be before the environment
+from .NoiseMap import NoiseMap
+
 # Must be before the generators
 from .Environment import Environment
 
+# Base generator
+from .BaseGenerator import BaseGenerator
+
 # Generators
 from .TerrainGenerator import TerrainGenerator
+from .TreeGenerator import TreeGenerator
